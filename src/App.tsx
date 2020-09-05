e1c73301dea446382065923f2209facd44a6316b
kochatkov <vlad.kochatkov@gmail.com>
2020-09-05 11:26:14 +0300

    filterByImage and filterByType is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,98 @@
-import React, { useEffect, useState } from "react";
-// import { getDataFromServer, getPhotos } from "./api";
-import RecipeReviewCard from "./demo";
-import "./App.scss";
+import React, { useEffect, useState } from 'react';
+import ReviewCard from './ReviewCard';
+import './App.scss';
+import { Photo, Data } from './Photo';
+import CircularIndeterminate from './Spinner';
+import CheckboxSelect from './CheckboxSelect';
+import MultipleSelect from './MultipleSelect';
 
 const API_URL = 'https://pixabay.com/api/?key=18049814-e9719222073fd5b9704ba5084';
 
 function App() {
-    const [photos, getPhotos] = useState([]);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [count, setCount] = useState(2);
+  const [filteredPhotos, setFilteredPhotos] = React.useState<Photo[]>([]);
 
-    useEffect(() => {
-        fetch(API_URL)
-        .then(res => res.json())
-            .then(data => {
-               const photos = Object.values(data)
-               // @ts-ignore
-                return getPhotos(photos[2]);
-            });
-    }, [])
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data: Data) => setPhotos(data.hits));
+  }, []);
 
-    console.log(photos)
+  useEffect(() => {
+    setFilteredPhotos([...photos]);
+  }, [photos]);
 
-    return (
-        <div className="App">
-            {photos.length !== 0 ?
-                photos.map((photo: any) => (
-                    <RecipeReviewCard
-                        key={photo.id}
-                        userImage={photo.userImageURL}
-                        user={photo.user}
-                        preview={photo.previewURL}
-                        tags={photo.tags}
-                        favorites={photo.favorites}
-                        downloads={photo.downloads}
-                        comments={photo.comments}
-                    />
-                )) : 'loading'
-            }
+  const loadMore = () => {
+    let counter = count;
+    fetch(`${API_URL}/&page=${count}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const morePhotos = [...photos, ...data.hits];
+        setPhotos(morePhotos);
+      });
 
-        </div>
-    );
+    counter++;
+    setCount(counter);
+  };
+
+  console.log(filteredPhotos);
+
+  const filterImageByCategory = (categories: string[]) => {
+    categories.map((category) => {
+      fetch(`${API_URL}/&category=${category}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setPhotos(data.hits);
+        });
+    });
+  };
+
+  const filterImageByType = (types: string[]) => {
+    types.map((type: string) => {
+      switch (type) {
+        case 'photo':
+          setFilteredPhotos([...photos].filter((photo) => photo.type === 'photo'));
+          break;
+        case 'illustration':
+          setFilteredPhotos([...photos].filter((photo) => photo.type === 'illustration'));
+          break;
+        case 'vector':
+          setFilteredPhotos([...photos].filter((photo) => photo.type === 'vector'));
+          break;
+        default:
+          setFilteredPhotos([...photos]);
+      }
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="App__selects">
+        <CheckboxSelect
+          // @ts-ignore
+          filterImageByType={filterImageByType}
+        />
+        <MultipleSelect
+          // @ts-ignore
+          filterImageByCategory={filterImageByCategory}
+        />
+        <input type="text" className="App__search" />
+      </div>
+      <div className="App__cards">
+        {filteredPhotos.length !== 0 ? (
+          filteredPhotos.map((photo) => <ReviewCard key={photo.id} photo={photo} />)
+        ) : (
+          <CircularIndeterminate />
+        )}
+      </div>
+      <div className="App__button-container">
+        <button onClick={loadMore} className="App__load-more">
+          Load more
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
